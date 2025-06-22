873144f7fb4721210eabaaa7ec2a0d110c266cbc
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-23 01:43:12 +0530

    Created basic server

---
+++ server/config/env.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  dev: {
+    db: process.env.DB_URI,
+    port: process.env.PORT || 3000,
+    key: process.env.JWT_KEY || "abcde12345",
+    log: "bug",
+  },
+  test: {
+    db: process.env.DB_URI,
+    port: process.env.PORT || 3000,
+    key: process.env.JWT_KEY || "abcde12345",
+    log: "info",
+  },
+  production: {
+    db: process.env.DB_URI,
+    port: process.env.PORT || 3000,
+    key: process.env.JWT_KEY || "abcde12345",
+    log: "error",
+  },
+};
+
+const stage = process.env.STAGE || "dev";
+export default config[stage];
