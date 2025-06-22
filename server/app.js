0bbbc500af5c9094cf53bccd5dfd23b2a26175aa
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-23 03:15:14 +0530

    Created basic login and signup functionality with hashing algorithm with form validation

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 import express from "express";
 import path from "path";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import flash from "connect-flash";
 
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(
+  session({ secret: "my-secret", resave: true, saveUninitialized: false })
+);
+app.use(flash());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(import.meta.dirname, "..", "client", "views"));
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,10 @@
+import { loginSchema, signupSchema } from "../validator/form.validator.js";
+import { hashPassword, verifyPassword } from "../utils/hash.js";
+import { createUser, findUserByUsername } from "../services/user.services.js";
+
 export const getSignupPage = (req, res) => {
   try {
-    return res.status(200).render("signup");
+    return res.status(200).render("signup", { errors: req.flash("errors") });
   } catch (err) {
     return res.status(404).render("pageNotFound");
   }
@@ -8,21 +12,71 @@ export const getSignupPage = (req, res) => {
 
 export const getLoginPage = (req, res) => {
   try {
-    return res.status(200).render("login");
+    return res.status(200).render("login", { errors: req.flash("errors") });
   } catch (err) {
     return res.status(404).render("pageNotFound");
   }
 };
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
   try {
+    const { data, error } = signupSchema.safeParse(req.body);
+
+    if (error) {
+      req.flash("errors", error.errors[0].message);
+      return res.status(400).redirect("/signup");
+    }
+
+    const { name, userName, email, password } = data;
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await createUser({
+      name,
+      userName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (!newUser) {
+      req.flash("errors", "User not created");
+      return res.status(400).redirect("/signup");
+    }
+
+    return res.status(201).redirect("/login");
   } catch (err) {
     return res.status(400).render("Something went wrong");
   }
 };
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
   try {
+    const { data, error } = loginSchema.safeParse(req.body);
+
+    if (error) {
+      req.flash("errors", error.errors[0].message);
+      return res.status(400).redirect("/login");
+    }
+
+    const { userName, password } = data;
+
+    const userData = await findUserByUsername(userName);
+
+    if (!userData) {
+      req.flash("errors", "Wrong User Name or Password");
+      return res.status(400).redirect("/login");
+    }
+
+    const checkPassword = await verifyPassword({
+      hashedPassword: userData.password,
+      password,
+    });
+
+    if (!checkPassword) {
+      req.flash("errors", "Wrong User Name or Password");
+      return res.status(400).redirect("/login");
+    }
+
+    return res.status(200).redirect("/");
   } catch (err) {
     return res.status(400).render("Something went wrong");
   }
