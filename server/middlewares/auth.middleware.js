5e47b98ef20e513334ef36a6d79b2914355036dd
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-23 05:38:19 +0530

    added documentation by adding comments

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,3 +1,4 @@
+// importing all modules and files
 import { getSessionById } from "../services/session.service.js";
 import { getUserById } from "../services/user.services.js";
 import { validate_login_with_cookies } from "../utils/cookie.js";
@@ -7,12 +8,14 @@ const authMiddleware = async (req, res, next) => {
   const accessToken = req.cookies.access_token;
   const refreshToken = req.cookies.refresh_token;
 
+  // if access token exists
   if (accessToken) {
     const decodedAccessToken = verifyToken(accessToken);
     req.user = decodedAccessToken;
     return next();
   }
 
+  // if refresh token exists and its valid but not access token
   if (refreshToken) {
     const decodedRefreshToken = verifyToken(refreshToken);
     const currentSession = await getSessionById(decodedRefreshToken.sessionId);
